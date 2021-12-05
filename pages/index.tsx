a5ce53c9d46eb6427cbecdd4cacbc07705f29521
Vivek Nigam <viveknigam.nigam3@gmail.com>
2021-12-05 08:40:56 +0000

    fix: checking for metamask

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,62 @@
-import { Box, Flex, Grid } from "@chakra-ui/layout";
+import { Box, Center, Flex, Grid, Text } from "@chakra-ui/layout";
 import { GridItem } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import Header from "../components/Header";
 import MainChessboard from "../components/MainChessboard";
 import UserDetails from "../components/UserDetails";
 
 const Home = () => {
+  const [hasMetamask, setHasMetamask] = useState(true);
   const [cookie, setCookie, removeCookie] = useCookies(["user"]);
 
   useEffect(() => {
-    if (typeof window.ethereum !== undefined) {
-      const ethereum = window.ethereum;
-      console.log(ethereum.selectedAddress);
-      ethereum.on("accountsChanged", (accounts) => {
-        if (accounts[0]) {
-          setCookie("user", JSON.stringify(accounts[0]), {
-            path: "/",
-            sameSite: true,
-          });
-        } else {
-          removeCookie("user");
-        }
-      });
+    if (typeof window !== undefined) {
+      if (typeof window.ethereum !== undefined) {
+        const ethereum = window.ethereum;
+        ethereum.on("accountsChanged", (accounts) => {
+          if (accounts[0]) {
+            setCookie("user", JSON.stringify(accounts[0]), {
+              path: "/",
+              sameSite: true,
+            });
+          } else {
+            removeCookie("user");
+          }
+        });
+      } else {
+        setHasMetamask(false);
+      }
     }
   }, []);
 
   return (
     <Box>
-      <Grid templateColumns="repeat(12, 1fr)" gap="16px">
-        <Header />
-        <MainChessboard />
-        <GridItem colStart={8} colSpan={3} width="100%" height="100%">
-          <Flex
-            direction="column"
-            height="100%"
-            border="1px solid #bbb"
-            borderRadius="10px"
-            margin="4"
-          >
-            <UserDetails hash={cookie.user} flexBasis="50%" p="4" />
-            <UserDetails hash={cookie.user} flexBasis="50%" p="4" />
-          </Flex>
-        </GridItem>
-      </Grid>
+      {hasMetamask ? (
+        <Grid templateColumns="repeat(12, 1fr)" gap="16px">
+          <Header />
+          <MainChessboard />
+          <GridItem colStart={8} colSpan={3} width="100%" height="100%">
+            <Flex
+              direction="column"
+              height="100%"
+              border="1px solid #bbb"
+              borderRadius="10px"
+              margin="4"
+            >
+              <UserDetails hash={cookie.user} flexBasis="50%" p="4" />
+              <UserDetails hash={cookie.user} flexBasis="50%" p="4" />
+            </Flex>
+          </GridItem>
+        </Grid>
+      ) : (
+        <Center>
+          <Text fontSize="2rem" maxW="60%">
+            No Metamask Wallet Installed. Please install metamask wallet to
+            continue
+          </Text>
+        </Center>
+      )}
     </Box>
   );
 };
