6dde6e5e442f330ba3cdb4c162967fce7d4ce2be
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-12-10 23:24:51 +0530

    feat: updated contract

--- contracts/Asharfi.sol
+++ contracts/Asharfi.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Asharfi is ERC20 {
-    mapping(address => uint256) balances;
-
     constructor() ERC20("Asharfi", "ASHF") {
-        _mint(msg.sender, 10000 * 10**decimals());
+        _mint(msg.sender, 10000 * (10**decimals()));
     }
 }
