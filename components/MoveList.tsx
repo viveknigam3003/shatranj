2edf9c4e6cae4ab398e46be5b8ba9d1a132148d3
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-12-11 11:00:07 +0530

    feat: updating styling

---
+++ components/MoveList.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+interface MoveListProps {
+  pgn: string;
+}
+
+const MoveList: React.FC<MoveListProps> = ({ pgn }) => {
+  return <Box>
+
+  </Box>;
+};
+
+export default MoveList;
--- components/UserDetails.tsx
+++ components/UserDetails.tsx
@@ -1,29 +1,28 @@
-import { Avatar } from "@chakra-ui/avatar";
 import { Box, BoxProps, HStack, Text } from "@chakra-ui/layout";
 import { Tag } from "@chakra-ui/react";
 import React from "react";
+import { truncateHash } from "../pages/play";
 
 interface Props {
   account: string;
   username?: string;
-  image?: string;
 }
 
 const UserDetails: React.FC<Props & BoxProps> = ({
   username = "User",
-  image = "",
   account,
   ...props
 }) => {
+  const truncatedAccount = truncateHash(account);
+
   return (
     <Box {...props}>
       <HStack alignItems="flex-start" justifyContent="flex-start" height="100%">
-        <Avatar name={username} src={image} />
         <Box spacing={1} maxWidth="50%">
           <Text fontWeight="600">{username}</Text>
           <Tag variant="subtle" colorScheme="whiteAlpha">
             <Text fontSize="0.8rem" isTruncated>
-              {account}
+              {truncatedAccount}
             </Text>
           </Tag>
         </Box>
