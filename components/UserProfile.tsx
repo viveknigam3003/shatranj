adf3bc0f2e7669c376777957479d331155c99882
viveknigam3003 <viveknigam.nigam3@gmail.com>
2021-12-11 10:26:50 +0530

    feat: styled userhash

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,21 +1,20 @@
 import { Box, HStack, Text } from "@chakra-ui/layout";
+import { Tag, TagLeftIcon } from "@chakra-ui/react";
+import { FaChessKnight } from "react-icons/fa";
 import React from "react";
 
 const UserProfile: React.FC<{ user: string }> = ({ user }) => {
   return (
-    <HStack
-      alignItems="center"
-      justifyContent="center"
-      height="100%"
-      borderRadius="10px"
-      bg="blue.500"
-    >
+    <HStack alignItems="center" justifyContent="center" height="100%">
       {/* <Avatar name={user} src={user} /> */}
       <Box spacing={1} maxWidth="50%">
         {/* <Text fontWeight="600">{user.username}</Text> */}
-        <Text fontSize="0.8rem" isTruncated>
-          {user}
-        </Text>
+        <Tag size="lg" variant="subtle" colorScheme="whiteAlpha">
+          <TagLeftIcon boxSize="12px" as={FaChessKnight} />
+          <Text fontSize="0.8rem" isTruncated>
+            {user}
+          </Text>
+        </Tag>
       </Box>
     </HStack>
   );
